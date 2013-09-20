2235640daf2555cb0727ef40806727e5e80f776b
Andrej Hoos <andrej.hoos@gmail.com>
2013-09-20 12:14:52 +0100

    comment non-core code

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 
+// Init app instance
 $app = require "./core/app.php";
 
+// Get all users from DB, eager load all fields using '*'
 $users = User::find($app->db,'*');
 
+// Render view 'views/index.php' and pass users variable there
 $app->renderView('index', array(
 	'users' => $users
 ));
--- models/user.php
+++ models/user.php
@@ -1,9 +1,16 @@
 <?php
 
+/**
+ * User model
+ */
 class User extends BaseModel{
 	
+	// Define neccessary constansts so we know from which table to load data
 	const tableName = 'users';
+	// ClassName constant is important for find and findOne static functions to work
 	const className = 'User';
+	
+	// Create getter functions
 	
 	public function getName() {
 		return $this->getField('name');
