4b7915a9e69d8cae6379fb8faef55010538117cb
Ján Mochňak <janmochnak@gmail.com>
2013-11-20 23:53:52 +0100

    Replaced short php tags with normals in index view.

--- views/index.php
+++ views/index.php
@@ -9,26 +9,26 @@
 		</tr>
 	</thead>
 	<tbody>
-		<?foreach($users as $user){?>
+		<?php foreach($users as $user): ?>
 		<tr>
-			<td><?=$user->getName()?></td>
-			<td><?=$user->getEmail()?></td>
-			<td><?=$user->getCity()?></td>
+			<td><?= $user->getName() ?></td>
+			<td><?= $user->getEmail() ?></td>
+			<td><?= $user->getCity() ?></td>
 		</tr>
-		<?}?>
+		<?php endforeach ?>
 	</tbody>
-</table>				
+</table>
 
 <form method="post" action="create.php">
-	
+
 	<label for="name">Name:</label>
 	<input name="name" input="text" id="name"/>
-	
+
 	<label for="email">E-mail:</label>
 	<input name="email" input="text" id="email"/>
-	
+
 	<label for="city">City:</label>
 	<input name="city" input="text" id="city"/>
-	
+
 	<button>Create new row</button>
 </form>
