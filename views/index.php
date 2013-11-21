df49a08905521df97729d13adeb459d8de73d7ff
Ján Mochňak <janmochnak@gmail.com>
2013-11-21 20:14:54 +0100

    Created base layout with bootstrap.

--- views/index.php
+++ views/index.php
@@ -1,34 +1,71 @@
-<h1>PHP Test Application</h1>
+<div class="row">
 
-<table>
-	<thead>
-		<tr>
-			<th>Name</th>
-			<th>E-mail</th>
-			<th>City</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php foreach($users as $user): ?>
-		<tr>
-			<td><?= $user->getName() ?></td>
-			<td><?= $user->getEmail() ?></td>
-			<td><?= $user->getCity() ?></td>
-		</tr>
-		<?php endforeach ?>
-	</tbody>
-</table>
+    <div id="header" class="col-md-12">
+        <div class="page-header">
+            <h1>PHP Test Application <small>Users List</small></h1>
+        </div>
+    </div>
 
-<form method="post" action="create.php">
+    <div id="main" class="col-md-8">
 
-	<label for="name">Name:</label>
-	<input name="name" input="text" id="name"/>
+    <div class="row filter">
+        <div class="col-md-4 col-md-push-8">
+              <input type="text" class="form-control" placeholder="Enter a City for filtering..">
+        </div>
+    </div>
+    <div class="table-responsive">
+    <table class="table table-striped table-hover table-bordered">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>E-mail</th>
+                <th>City</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($users as $user): ?>
+            <tr>
+                <td><?= $user->getName() ?></td>
+                <td><?= $user->getEmail() ?></td>
+                <td><?= $user->getCity() ?></td>
+            </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+    </div>
+    </div>
 
-	<label for="email">E-mail:</label>
-	<input name="email" input="text" id="email"/>
+<div id="sidebar" class="col-md-4">
 
-	<label for="city">City:</label>
-	<input name="city" input="text" id="city"/>
+   <div class="well">
 
-	<button>Create new row</button>
-</form>
+       <h3 class="well-header"><span class="glyphicon glyphicon-user"></span> Add New User</h3>
+
+        <form method="post" action="create.php" class="form form-horizontal">
+
+            <div class="form-group">
+                <label for="name" class="col-sm-3 control-label">Name:</label>
+                <div class="col-sm-9">
+                    <input name="name" input="text" class="form-control" id="name" />
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="email" class="col-sm-3 control-label">Email:</label>
+                <div class="col-sm-9">
+                    <input name="email" input="email" class="form-control" id="email" />
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="city" class="col-sm-3 control-label">City:</label>
+                <div class="col-sm-9">
+                    <input name="city" input="text" class="form-control" id="city" />
+                </div>
+            </div>
+
+            <input type="submit" class="btn btn-primary btn-lg btn-block" value="Create" />
+        </form>
+    </div>
+
+</div>
--- views/layout.php
+++ views/layout.php
@@ -1,27 +1,24 @@
 <!DOCTYPE html>
 <html>
   <head>
-  	<meta charset="UTF-8">
-  	
+    <meta charset="UTF-8">
+
     <title>PHP Test Application</title>
-    
-	<link href="favicon.ico" type="image/x-icon" rel="icon" />
-	<link href="favicon.ico" type="image/x-icon" rel="shortcut icon" />	
-    
+    <link href="favicon.ico" type="image/x-icon" rel="icon" />
+    <link href="favicon.ico" type="image/x-icon" rel="shortcut icon" />
+
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/application.css">
-    
-    <script type="text/javascript" charset="utf-8" src="js/jquery.min.js"></script>
-	<script type="text/javascript" charset="utf-8" src="js/bootstrap.min.js"></script>
 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script type="text/javascript" charset="utf-8" src="js/angular.min.js"></script>
+    <script type="text/javascript" charset="utf-8" src="js/jquery.min.js"></script>
+    <script type="text/javascript" charset="utf-8" src="js/bootstrap.min.js"></script>
   </head>
   <body>
-
   <div class="container">
-    
     <?= $content ?>
-    
-  </div>  
-
+  </div>
   </body>
 </html>
