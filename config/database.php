448dca51189a9fc31cf70aac80dc3669c8cc5494
Tomas Novella <tomasnovella@gmail.com>
2020-01-25 22:17:40 +0100

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,14 @@
+
+<?php
+
+/*
+* This is template of database config.
+* Fill out database configuration options below and rename this file to 'database.php'
+*/
+
+$database = array(
+	'address' 	=> 'onnjomlc4vqc55fw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+	'username'	=> 'n7o3x6umwljqmgr1',
+	'password'	=> 'khkg3qlzlz0mc1aw',
+	'database'	=> 'bnash03x0mip6hdu'
+);
