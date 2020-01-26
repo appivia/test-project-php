39c31d3338765dde573200bcbb9fd2aedd0cf0f2
tomasn <tomasnovella@gmail.com>
2020-01-26 20:31:03 +0100

    Style form using bootstrap

--- views/index.php
+++ views/index.php
@@ -1,6 +1,29 @@
-<h1>PHP Test Application</h1>
+<h1 class="text-center">PHP Test Application</h1>
 
-<table>
+<div class="container-fluid">
+<form class="form-horizontal" method="post" action="create.php">
+	<div class="form-group">
+		<label class="control-label col-sm-5" for="name">Name:</label>
+		<div class="col-sm-3"><input name="name" input="text" id="name" class="form-control"/></div>
+	</div>
+
+	<div class="form-group">
+		<label class="control-label col-sm-5" for="email">E-mail:</label>
+		<div class="col-sm-3"><input name="email" type="email" input="text" id="email" class="form-control"/></div>
+	</div>
+
+	<div class="form-group">
+		<label class="control-label col-sm-5" for="city">City:</label>
+		<div class="col-sm-3"><input name="city" input="text" id="city" class="form-control"/></div>
+	</div>
+    <div class="form-group">
+		<div class="col-sm-4 col-sm-offset-5">
+			<button class="btn btn-default">Create new row</button>
+		</div>
+	</div>
+</form>
+
+<table class="table table-striped">
 	<thead>
 		<tr>
 			<th>Name</th>
@@ -18,17 +41,4 @@
 		<?php } ?>
 	</tbody>
 </table>
-
-<form method="post" action="create.php">
-
-	<label for="name">Name:</label>
-	<input name="name" input="text" id="name"/>
-
-	<label for="email">E-mail:</label>
-	<input name="email" input="text" id="email"/>
-
-	<label for="city">City:</label>
-	<input name="city" input="text" id="city"/>
-
-	<button>Create new row</button>
-</form>
+</div>
