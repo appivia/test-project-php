b565db4b89948358bcbcbf17f9d448816f0a9bc5
Fotis Alexandrou <falexandrou@gmail.com>
2022-11-27 23:59:14 +0200

    enhancement: use JsonSerializable interface for errors & users

--- models/user.php
+++ models/user.php
@@ -3,25 +3,33 @@
 /**
  * User model
  */
-class User extends BaseModel{
-	
+class User extends BaseModel implements JsonSerializable
+{
 	// Define neccessary constansts so we know from which table to load data
 	const tableName = 'users';
 	// ClassName constant is important for find and findOne static functions to work
 	const className = 'User';
-	
+
 	// Create getter functions
-	
+
 	public function getName() {
 		return $this->getField('name');
 	}
-	
+
 	public function getEmail() {
 		return $this->getField('email');
 	}
-	
+
 	public function getCity() {
 		return $this->getField('city');
 	}
-	
+
+	public function jsonSerialize()
+	{
+		return [
+			'name' => $this->getName(),
+			'email' => $this->getEmail(),
+			'city' => $this->getCity(),
+		];
+	}
 }
