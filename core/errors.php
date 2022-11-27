02bee5ed8d3c2a17dbc084ebb0e2b9500f2221da
Fotis Alexandrou <falexandrou@gmail.com>
2022-11-28 00:03:20 +0200

    feat: render user creation and errors as json

---
+++ core/errors.php
@@ -0,0 +1,36 @@
+<?php
+
+class ValidationError extends Exception implements JsonSerializable
+{
+	/**
+	 * @var {Array} the errors for every form element
+	 */
+	private $_errors = [];
+
+	/**
+	 * Constructs a validation error by providing a message and a list of errors
+	 * as an associative array
+	 *
+	 * param $message: The overall error message
+	 * param $errors: The error message per field
+	 */
+	public function __construct($message, $errors = [])
+	{
+		parent::__construct($message);
+		$this->_errors = $errors;
+	}
+
+	public function getErrors()
+	{
+		return $this->_errors;
+	}
+
+	public function jsonSerialize()
+	{
+		return [
+			'error' => true,
+			'message' => $this->message,
+			'errors' => $this->getErrors(),
+		];
+	}
+}
--- create.php
+++ create.php
@@ -1,15 +1,64 @@
 <?php
+// The page was accessed directly, bail...
+if (
+	!isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+	|| strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest'
+) {
+  header('Location: /index.php');
+  die;
+}
+
+require_once './core/errors.php';
+
+/**
+ * Returns the sanitized user data
+ */
+function getSanitizedData() {
+	$args = [
+		'name' => FILTER_SANITIZE_STRING,
+		'email' => FILTER_SANITIZE_EMAIL,
+		'city' => FILTER_SANITIZE_STRING,
+	];
+
+	return filter_var_array($_POST, $args, FILTER_NULL_ON_FAILURE);
+}
+
+function validateData($data) {
+	$errors = [];
+
+	if (empty($data['name'])) {
+		$errors['name'] = 'Please provide the name for the user';
+	}
+
+	if (empty($data['email'])) {
+		$errors['email'] = 'Please provide the user’s e-mail address';
+	} else if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL, FILTER_VALIDATE_EMAIL)) {
+		$errors['email'] = 'The email you provided is not valid';
+	}
+
+	if (empty($data['city'])) {
+		$errors['city'] = 'Please provide a city for the user';
+	}
+
+	if (!empty($errors)) {
+		throw new ValidationError(
+			'There are errors in the form, please address them and try again', $errors
+		);
+	}
+
+	return $data;
+}
 
 $app = require "./core/app.php";
 
-// Create new instance of user
-$user = new User($app->db);
-// Insert it to database with POST data
-$user->insert(array(
-	'name' => $_POST['name'],
-	'email' => $_POST['email'],
-	'city' => $_POST['city']
-));
+$data = getSanitizedData();
 
-// Redirect back to index
-header('Location: index.php');
+try {
+	$valid = validateData($data);
+	$user = new User($app->db);
+	$user->insert($valid);
+
+	$app->renderJson($user);
+} catch (ValidationError $e) {
+	$app->renderJson($e, 422);
+}
