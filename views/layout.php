092a726359f0af1441682e59c8383273381364b0
Fotis Alexandrou <falexandrou@gmail.com>
2022-11-27 19:56:19 +0200

    chore: update to latest bootstrap version & jquery, use cdn

--- views/layout.php
+++ views/layout.php
@@ -2,26 +2,25 @@
 <html>
   <head>
   	<meta charset="UTF-8">
-  	
-    <title>PHP Test Application</title>
-    
-	<link href="favicon.ico" type="image/x-icon" rel="icon" />
-	<link href="favicon.ico" type="image/x-icon" rel="shortcut icon" />	
-    
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/application.css">
-    
-    <script type="text/javascript" charset="utf-8" src="js/jquery.min.js"></script>
-	<script type="text/javascript" charset="utf-8" src="js/bootstrap.min.js"></script>
 
+    <title>PHP Test Application</title>
+
+	<link href="favicon.ico" type="image/x-icon" rel="icon" />
+	<link href="favicon.ico" type="image/x-icon" rel="shortcut icon" />
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/application.css">
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.slim.min.js" integrity="sha512-yBpuflZmP5lwMzZ03hiCLzA94N0K2vgBtJgqQ2E1meJzmIBfjbb7k4Y23k2i2c/rIeSUGc7jojyIY5waK3ZxCQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </head>
   <body>
 
-  <div class="container">
-    
+  <main class="container">
+
     <?= $content ?>
-    
-  </div>  
+
+  </main>
 
   </body>
 </html>
